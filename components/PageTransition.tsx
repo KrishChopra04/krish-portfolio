16b5ca65e5c610ac880f89ebeb10ccd9c9293e1c
KrishChopra04 <krish.12chopra@gmail.com>
2026-04-02 15:29:22 -0400

    Update PageTransition.tsx

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -12,7 +12,6 @@ export default function PageTransition({ children }: { children: React.ReactNode
         key={pathname}
         initial={{ opacity: 0, y: 8 }}
         animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -8 }}
         transition={{ duration: 0.3, ease: 'easeOut' }}
       >
         {children}
