0358ece2736021403081953b1e9ae9ab34cc7570
KrishChopra04 <krish.12chopra@gmail.com>
2026-04-01 23:58:31 -0400

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,6 @@ export default function Contact() {
   return (
     <div className="two-col" style={{
       display: 'grid',
-      gridTemplateColumns: '1fr 1fr',
       minHeight: 'calc(100vh - var(--nav-h))',
     }}>
       <div style={{
