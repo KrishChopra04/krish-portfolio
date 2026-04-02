e8206a19ea658129c004cdbaf189defeadadb032
KrishChopra04 <krish.12chopra@gmail.com>
2026-04-02 15:23:26 -0400

    Update PageTransition.tsx

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -5,9 +5,9 @@ import { motion } from 'framer-motion'
 export default function PageTransition({ children }: { children: React.ReactNode }) {
     return (
         <motion.div
-            initial={{ opacity: 0, y: 8 }}
+            initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.3, ease: 'easeOut' }}
+            transition={{ duration: 0.7, ease: 'easeOut' }}
            >
             {children}
            </motion.div> 
